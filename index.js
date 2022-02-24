00bede06bc0d49db9b4683af5ae13300e086e560
Pab-lo <salaberry.pablo@gmail.com>
2022-02-24 18:00:49 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+letsaleEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
